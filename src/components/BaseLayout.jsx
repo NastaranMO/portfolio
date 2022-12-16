19ac0e7866ed44f769a7d47da940374bf716fe3a
Nastaran Moghadasi <nastaran.moghadasi@gmail.com>
2022-12-16 15:35:39 +0100

    create navbar

--- src/components/BaseLayout.jsx
+++ src/components/BaseLayout.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './BaseLayout.module.scss'
+import Navbar from './Navbar';
 import { Route, Routes } from "react-router-dom";
 import { Box, Grid } from '@mui/material'
 
@@ -8,7 +9,16 @@ const BaseLayout = () => {
         <Box className='dark'>
             <Grid container display={'flex'} flexDirection={'column'} minHeight={'100vh'}
                 justifyContent={'space-between'}>
-
+                <Grid item>
+                    <Navbar />
+                </Grid>
+                <Grid item flexGrow={1}>
+                    <Routes>
+                        {/* <Route exact path={'/'} element={<Home />} />
+                        <Route exact path={'/about'} element={<About />} />
+                        <Route exact path={'/portfolio'} element={<Portfolio />} /> */}
+                    </Routes>
+                </Grid>
                 <Grid item>
                     <Box component={'footer'} display={'flex'} flexDirection={'column'} alignItems={'center'}
                         py={'1.5rem'} sx={{ opacity: 0.7 }} width={'100%'}>
