571efb59497daa1664d46e4d6a0098341b6d107d
Nastaran Moghadasi <nastaran.moghadasi@gmail.com>
2022-12-16 15:33:47 +0100

    add footer info

--- src/components/BaseLayout.jsx
+++ src/components/BaseLayout.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
-import { Box } from '@mui/material'
+import './BaseLayout.module.scss'
+import { Route, Routes } from "react-router-dom";
+import { Box, Grid } from '@mui/material'
 
 const BaseLayout = () => {
     return (
-        <Box></Box>
+        <Box className='dark'>
+            <Grid container display={'flex'} flexDirection={'column'} minHeight={'100vh'}
+                justifyContent={'space-between'}>
+
+                <Grid item>
+                    <Box component={'footer'} display={'flex'} flexDirection={'column'} alignItems={'center'}
+                        py={'1.5rem'} sx={{ opacity: 0.7 }} width={'100%'}>
+                        <p>Created with &hearts; by Nastaran Moghadasi</p>
+                        <p>&copy; 2022</p>
+                    </Box>
+                </Grid>
+            </Grid>
+        </Box>
     )
 }
 
